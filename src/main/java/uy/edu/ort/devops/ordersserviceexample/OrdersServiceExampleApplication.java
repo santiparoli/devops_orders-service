c07351e6149983e8938f0dae056d501d60e688e3
Santiago Paroli <73951110+santiparoli@users.noreply.github.com>
2022-12-01 18:05:56 -0300

    .

--- src/main/java/uy/edu/ort/devops/ordersserviceexample/OrdersServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/ordersserviceexample/OrdersServiceExampleApplication.java
@@ -1,5 +1,6 @@
 package uy.edu.ort.devops.ordersserviceexample;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
